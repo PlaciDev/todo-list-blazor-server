713e7d6e0a8895262aa3255383a31b553534c836
PlaciDev <brunoplacidev@gmail.com>
2025-10-08 12:58:02 -0300

    realiza o mapeamento fluente de User

---
+++ ToDoListBlazorServer/Data/Mappings/UserMap.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ToDoListBlazorServer.Models;
+
+namespace ToDoListBlazorServer.Data.Mappings
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(50);
+
+            builder.Property(x => x.Email)
+                .IsRequired()
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(100);
+
+            builder.Property(x => x.PasswordHash)
+                .IsRequired()
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(255);
+
+            builder.HasMany(x => x.TaskItems)
+                .WithOne(x => x.User)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+
+
+
+        }
+    }
+}

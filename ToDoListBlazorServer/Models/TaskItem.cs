3f98b6bac0c6b3be47e70bf674b23bd69dc8f4ce
PlaciDev <brunoplacidev@gmail.com>
2025-10-08 12:18:28 -0300

    define os campos e relacionamentos, dos modelos TaskItem e User

---
+++ ToDoListBlazorServer/Models/TaskItem.cs
@@ -0,0 +1,15 @@
+﻿namespace ToDoListBlazorServer.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}

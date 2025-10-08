203bf6cfcec373a0a3e82d1b850631a62457d936
PlaciDev <brunoplacidev@gmail.com>
2025-10-08 13:10:46 -0300

    realiza o mapemanto fluente de TaskItem

---
+++ ToDoListBlazorServer/Data/Mappings/TaskItemMap.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ToDoListBlazorServer.Models;
+
+namespace ToDoListBlazorServer.Data.Mappings
+{
+    public class TaskItemMap : IEntityTypeConfiguration<TaskItem>
+    {
+        public void Configure(EntityTypeBuilder<TaskItem> builder)
+        {
+            builder.ToTable("TaskItem");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property(x => x.Title)
+                .IsRequired()
+                .HasColumnName("Title")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(100);
+
+            builder.Property(x => x.Description)
+                .HasColumnName("Description")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(500);
+
+            builder.Property(x => x.IsCompleted)
+                .IsRequired()
+                .HasColumnName("IsCompleted")
+                .HasColumnType("BIT");
+
+            builder.Property(x => x.CreatedAt)
+                .IsRequired()
+                .HasColumnName("CreatedAt")
+                .HasColumnType("DATETIME2");
+
+            builder.HasOne(x => x.User)
+                .WithMany(x => x.TaskItems)
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}

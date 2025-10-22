42693f8b0b86432e1c87a4f598247d586afd3c75
PlaciDev <brunoplacidev@gmail.com>
2025-10-22 17:00:23 -0300

    Implementa autenticação e ajustes no modelo de usuário

--- ToDoListBlazorServer/Program.cs
+++ ToDoListBlazorServer/Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
 using ToDoListBlazorServer.Data;
 using ToDoListBlazorServer.Services;
+using ToDoListBlazorServer.Services.Auth;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,13 +14,27 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
 // Injeção de dependência do serviço TaskService
-
 builder.Services.AddScoped<TaskItemService>();
 
 // Confuguração do Entity Framework Core com SQL Server
-
 builder.Services.AddDbContext<AppDbContext>(option =>
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Injeção de dependência do serviço de autenticação
+
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<CustomAuthStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider =>
+    provider.GetRequiredService<CustomAuthStateProvider>());
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+   .AddCookie(option =>
+   {
+       option.LoginPath = "/login";
+       option.ExpireTimeSpan = TimeSpan.FromHours(1);
+   });
+
 
 var app = builder.Build();
 
@@ -34,6 +51,11 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+// Configuração do middleware de autenticação e autorização
+app.UseAuthentication();
+app.UseAuthorization();
+
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
